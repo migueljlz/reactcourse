c009bd6e504e7291224e1cd4a94bc7f3e3b3d70d
Miguel Jaramillo <miguel.jaramillo@neoris.com>
2022-05-05 13:40:28 -0500

    Proyect updated

---
+++ 02-intro-javascript/src/bases/01-const-js.js
@@ -0,0 +1,18 @@
+// Varaiables y Constantes
+
+const nombre = 'MK'; 
+const apellido = 'JL';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+// var No se debería de usar...
+if (true){
+    const nombre = 'Shaz';
+
+    console.log(nombre);
+}
+
+console.log(valorDado);
